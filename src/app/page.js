c96f2e843ff50814c212dbd3e25d55c62f51fd6c
Rafiikhwan <rafiikhwan2006@gmail.com>
2023-09-21 10:28:15 +0700

    Login Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import logo from '../../assets/image/logo90x90.png'
 
 export default function Home() {
   return (
-    <main>
-      
+    <main className="flex justify-center bg-black bg-opacity-90 min-h-screen">
+      <div className="flex flex-row flex-wrap  align-middle p-10 bg-gray-950 bg-opacity-50">
+        <Image 
+          src={logo} 
+          alt="Logo SDQ Bina Mulya" 
+        />
+        <div className="flex flex-col align-middle p-2">
+          <h3 className="text-white text-4xl">SDQ Bina Mulya</h3>
+          <p className='text-gray-600'>Aplikasi SPP</p>
+        </div>
+      </div>
     </main>
   )
 }
