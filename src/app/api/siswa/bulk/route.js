9654b1ce4ca69c60fb9a5c4168283e9b8cf1ef5d
nanand <61643931+nanandr@users.noreply.github.com>
2023-11-18 21:33:44 +0700

    bulk siswa post

---
+++ src/app/api/siswa/bulk/route.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server"
+
+export const POST = async (req) => {
+    const body = await req.json()
+    try {
+        const { siswa, tahunAjar } = body
+        await Promise.all(siswa.map(async (item, index) => {
+            const { nama, nis, kelasId, nisn, alamat, jk, angkatan, hp } = item
+            const data = await prisma.siswa.upsert({
+                where: {nisn: nisn, nis: nis},
+                update: {nama: nama, nis: nis, nisn: nisn, alamat: alamat, jk: jk, angkatan: angkatan, hp: hp, updatedAt: new Date()},
+                create: {nama: nama, nis: nis, nisn: nisn, alamat: alamat, jk: jk, angkatan: angkatan, hp: hp},
+            })
+
+            if(kelasId) {
+                const kelasSiswa = await prisma.kelasSiswa.upsert({
+                    where: { siswaId_tahunAjarId: { siswaId: parseInt(data.id), tahunAjarId: tahunAjar } },
+                    update: { kelasId: kelasId, updatedAt: new Date() },
+                    create: { siswaId: parseInt(data.id), kelasId: kelasId, tahunAjarId: tahunAjar }
+                })
+            }
+        }))
+
+        return NextResponse.json({message: "Successfully created data"}, {status: 201})
+    }
+    catch(error) {
+        console.log(error)
+        return NextResponse.json({message: "Error fetching data"}, {status: 500})
+    }
+}
