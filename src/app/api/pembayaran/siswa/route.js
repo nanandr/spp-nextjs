b88b6adf2ec877610cee86309e6a2edfab818ded
Rafiikhwan <rafiikhwan2006@gmail.com>
2023-12-07 17:13:27 +0700

    login siswa for mobile

--- src/app/api/pembayaran/siswa/route.js
+++ src/app/api/pembayaran/siswa/route.js
@@ -2,26 +2,68 @@ import { NextResponse } from "next/server"
 import { headers } from "next/headers"
 import { verifyJwt } from "@/lib/jwt"
 import { prisma } from "../../../../../utils/prisma"
+import { dateTimeFormat } from "../../../../../utils/format"
 
 export const GET = async (req, res) => {
     const url = new URL(req.url)
-    let tahunAjar = url.searchParams.get("tahun")
+    let tahun = url.searchParams.get("tahun")
     try {
         const headersList = headers()
         const authorization = headersList.get('authorization')
         const bearer = authorization.startsWith('Bearer ') ? authorization.slice(7, authorization.length) : null
-        const user = verifyJwt(bearer)
-        console.log(user)
+        const siswa = verifyJwt(bearer)
+        console.log(siswa)
+
+        const whereCondition = { where: { siswaId: siswa.id } }
+        const total = await prisma.transaksi.count(whereCondition)
 
         const pembayaran = await prisma.transaksi.findMany({
-            where: {
-                
+            ...whereCondition,
+            include: {
+                spp: true,
+                user: true,
+                siswa: {
+                    include: {
+                        kelas: {
+                            include: { kelas: true, tahunAjar: true },
+                            where: { tahunAjarId: tahun }
+                        }
+                    }
+                },
             }
         })
 
-        return NextResponse.json({ user: user })
+        let filterPembayaran = pembayaran
+
+        if (tahun) {
+            filterPembayaran = pembayaran.filter(item => {
+                return item.spp.tahunAjarId == tahun
+            })
+        }
+
+        const data = filterPembayaran.map(item => {
+            return {
+                id: parseInt(item.id),
+                namaSiswa: item.siswa.nama,
+                namaPetugas: item.user.nama,
+                tanggalBayar: dateTimeFormat(item.tanggal),
+                totalBayar: parseInt(item.totalBayar),
+                bulan: item.bulan,
+                spp: item.spp,
+                kelas: item.siswa.kelas.map(kelas => {
+                    return {
+                        namaKelas: kelas.kelas.namaKelas,
+                        tahunAjar: kelas.tahunAjar.tahun
+                    }
+                }),
+                createdAt: dateTimeFormat(item.createdAt),
+                updatedAt: dateTimeFormat(item.updatedAt)
+            }
+        })
+
+        return NextResponse.json({ transaksi: data, user: siswa })
     }
-    catch(err) {
+    catch (err) {
         console.log(err)
         return NextResponse.json({ message: err.message }, { status: 500 })
     }
